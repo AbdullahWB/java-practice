70aaf5925546eac03f58a91e7dca7baa9a8165e8
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-09-20 01:31:35 +0800

    first commit

---
+++ day-2/variable.java
@@ -0,0 +1,7 @@
+public class variable {
+    public static void main(String[] args){
+        System.out.println("hello world"); //this will print with a newline character
+        System.out.print("hello world\n"); //this will print normally and use \n to print with a newline character
+        System.out.print("hello world\n"); //this will print normally 
+    } 
+}
