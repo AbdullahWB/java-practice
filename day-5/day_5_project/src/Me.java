71ffc158c1296528de221eed1927d6e3336263cb
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-10-14 01:38:23 +0800

    project file proprely and variable

---
+++ day-5/day_5_project/src/Me.java
@@ -0,0 +1,8 @@
+public class Me {
+    public static void myName(){
+        System.out.println("My name is Abdullah!");
+    }
+    public static void myAge() {
+        System.out.println("I am 21 years old.");
+    }
+}
---
+++ day-5/day_5_project/src/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+        String myNameFast = "Mohammad";
+        String myNameLast = "Abdullah";
+        String myHartName = "sauda";
+        String copyName = myHartName;
+        myHartName = "binte";
+        System.out.println(copyName);
+        // myHartName = "bussrah";
+        System.out.println(myHartName);
+        String fullName = myNameFast + " " + myNameLast;
+        System.out.println(fullName);
+    }
+}
