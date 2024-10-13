618c3bb5c60a646946349a144e654d4e53057b2a
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-10-13 21:17:58 +0800

    me ffnc

---
+++ day-4/Me.java
@@ -0,0 +1,8 @@
+public class Me {
+    public static void sayName() {
+        System.out.println("My name is Abdullah");
+    }    
+    public static void sayAge(){
+        System.out.println("I am 21 years old");
+    }
+}
