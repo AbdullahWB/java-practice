311e68597652707efd2169d4fc8635813459d2cd
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-12-15 01:03:48 +0800

    enum

--- day-23/class_work/calssTest/src/Animal.java
+++ day-23/class_work/calssTest/src/Animal.java
@@ -1,14 +1,8 @@
-public enum Animal {
-    DOG,
-    CAT,
-    COW
-}
-
 interface Sound {
     void makeSound();
 }
 
-public enum Animal implements Sound {
+enum AnimalSound implements Sound {
     DOG {
         @Override
         public void makeSound() {
@@ -26,19 +20,14 @@ public enum Animal implements Sound {
         public void makeSound() {
             System.out.println("Moo! Moo!");
         }
-    };
-
-    // Main method to test the implementation
-    public static void main(String[] args) {
-        for (Animal animal : Animal.values()) {
-            System.out.print(animal.name() + " says: ");
-            animal.makeSound();
-        }
     }
 }
 
-public class InterfaceInEnums {
+public class Animal {
     public static void main(String[] args) {
-
+        System.out.println("Individual animal sound:");
+        AnimalSound.DOG.makeSound();
+        AnimalSound.CAT.makeSound();
+        AnimalSound.COW.makeSound();
     }
 }
